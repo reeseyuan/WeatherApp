54c0deda319e0bc506e80b28fa2b2a4c749a12a5
reeseyuan <yuanruisi0119@gmail.com>
2018-05-23 16:41:39 +0800

    basic finish

--- app/src/main/java/com/example/reese/weatherapp/WeatherActivity.java
+++ app/src/main/java/com/example/reese/weatherapp/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.example.reese.weatherapp;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.TestLooperManager;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -45,6 +47,16 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {   //获取控件
         super.onCreate(savedInstanceState);
+        /*
+        if(Build.VERSION.SDK_INT >= 21) {      //系统版本号判断，因为只有版本号21以后才可以使用
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);   //设置状态栏为透明色
+        }
+        */
+        
         setContentView(R.layout.activity_weather);
 
         //初始化各个控件
